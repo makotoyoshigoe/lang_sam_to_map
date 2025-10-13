e4337f3429ccb6d48c3bd3d35270fa40726a15d3
makotoyoshigoe <myoshigo0127@gmail.com>
2025-10-13 19:31:07 +0900

    add: lsa_nav_controller package and buildable

---
+++ lsa_nav_controller/include/lsa_nav_controller/lsa_nav_controller.hpp
@@ -0,0 +1,24 @@
+// SPDX-FileCopyrightText: 2025 Makoto Yoshigoe myoshigo0127@gmail.com 
+// SPDX-License-Identifier: Apache-2.0
+
+#include <rclcpp/rclcpp.hpp>
+#include <sensor_msgs/msg/laser_scan.hpp>
+#include <nav_msgs/msg/occupancy_grid.hpp>
+
+namespace lsa_nav_controller
+{
+class LsaNavController : public rclcpp::Node{
+    public:
+    LsaNavController();
+    ~LsaNavController();
+    void cb_lsa_map(nav_msgs::msg::OccupancyGrid::ConstSharedPtr msg);
+    void declare_param(void);
+    void init_param(void);
+    void init_pubsub(void);
+
+    private:
+    rclcpp::Subscription<nav_msgs::msg::OccupancyGrid>::SharedPtr sub_lsa_ma_;
+    rclcpp::Publisher<sensor_msgs::msg::LaserScan>::SharedPtr pub_road_scan_;
+};
+    
+} // namespace lsa_nav_controller
