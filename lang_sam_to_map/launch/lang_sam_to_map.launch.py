ae25280972434c1a01d263a05e8c9d37bbba8b8e
makotoyoshigoe <myoshigo0127@gmail.com>
2025-10-28 20:27:45 +0900

    delete: unused

--- lang_sam_to_map/launch/lang_sam_to_map.launch.py
+++ lang_sam_to_map/launch/lang_sam_to_map.launch.py
@@ -15,28 +15,12 @@ def generate_launch_description():
         'use_rviz',
         default_value='true',
         description='Set "true" to launch rviz.')
-    
     # パッケージ共有ディレクトリ取得
-    pkg_dir = get_package_share_directory('lang_sam_to_map')
-    lsa_pkg = get_package_share_directory('ros2_lang_sam')
     rs_pkg = get_package_share_directory('realsense2_camera')
+    pkg_dir = get_package_share_directory('lang_sam_to_map')
 
-    # RViz設定ファイルパス
-    rviz_config = os.path.join(pkg_dir, 'rviz', 'rviz.rviz')
-    
-    # パラメータファイルパス
-    param_dir = os.path.join(pkg_dir, 'param', 'param.yaml')
-    
     load_nodes = GroupAction(
         actions=[
-            IncludeLaunchDescription(
-                PythonLaunchDescriptionSource(
-                    os.path.join(lsa_pkg, 'launch', 'server.launch.py')
-                ), 
-                launch_arguments={
-                    'sam_type': 'sam2.1_hiera_large'
-                }.items()
-            ),
             IncludeLaunchDescription(
                 PythonLaunchDescriptionSource(
                     os.path.join(rs_pkg, 'launch', 'rs_launch.py')
@@ -50,27 +34,17 @@ def generate_launch_description():
                     'pointcloud.ordered_pc': 'false', 
                 }.items()
             ),
-            TimerAction(
-                period=1.0,
-                actions=[
-                    Node(
-                        package='lang_sam_to_map',
-                        executable='lang_sam_to_map',
-                        name='lang_sam_to_map',
-                        output='screen',
-                        parameters=[param_dir]
-                    )
-                ]
+            IncludeLaunchDescription(
+                PythonLaunchDescriptionSource(
+                    os.path.join(pkg_dir, 'launch', 'lang_sam_to_map_wo_camera.launch.py')
+                ),
+                launch_arguments={
+                    'use_rviz': use_rviz
+                }.items()
             )
-        ])
-    rviz2 = Node(package='rviz2',
-        executable='rviz2',
-        name='rviz2',
-        output='log',
-        arguments=['-d', rviz_config, '--ros-args', '--log-level', 'warn'],
-        condition=IfCondition(use_rviz))
+        ]
+    )
     ld = LaunchDescription()
     ld.add_action(declare_arg_use_rviz)
     ld.add_action(load_nodes)
-    ld.add_action(rviz2)
     return ld
